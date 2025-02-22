d60be23d114b8a5c43e5dd45c2b61a71762faf4a
BuiBuild <lei15557570906@outlook.com>
2025-02-23 00:36:57 +0800

    CTPTraderApiExecutor: 设置回调函数

--- src/Trader/EMS/CTPTraderApiExecutor.cc
+++ src/Trader/EMS/CTPTraderApiExecutor.cc
@@ -2,7 +2,7 @@
  * @Author: LeiJiulong
  * @Date: 2025-02-22 23:52:16
  * @LastEditors: LeiJiulong && lei15557570906@outlook.com
- * @LastEditTime: 2025-02-22 23:56:47
+ * @LastEditTime: 2025-02-23 00:29:19
  * @Description: 
  */
 #include "Trader/EMS/CTPTraderApiExecutor.h"
@@ -16,7 +16,31 @@ void CTPTraderApiExecutor::execute(const OrderRequest& orderReq)
 
 CTPTraderApiExecutor::CTPTraderApiExecutor(const std::string &configPath)
 {
-    // 初始化
-    
+    // 配置登陆信息
+    CSimpleIniA ini;
+    ini.LoadFile(configPath.c_str());
+    const char* brokerID = ini.GetValue("CTP", "BrokerID", "");
+    const char* userID = ini.GetValue("CTP", "UserID", "");
+    const char* password = ini.GetValue("CTP", "Password", "");
+    const char* frontAddr = ini.GetValue("CTP", "FrontAddr", "");
+    sprintf(pReqUserLoginField_.BrokerID, "%s", brokerID);
+    sprintf(pReqUserLoginField_.UserID, "%s", userID);
+    sprintf(pReqUserLoginField_.Password, "%s", password);
+    sprintf(frontAddr_, "%s", frontAddr);
+    traderApi_ = CThostFtdcTraderApi::CreateFtdcTraderApi();
+
     init();
+}
+void CTPTraderApiExecutor::init()
+{
+    traderApi_->RegisterSpi(this);
+    traderApi_->RegisterFront(frontAddr_);
+    traderApi_->Init();
+    static int requestId = 0;
+    traderApi_->ReqUserLogin(&pReqUserLoginField_, requestId++);
+}
+
+CTPTraderApiExecutor::~CTPTraderApiExecutor()
+{
+    
 }
